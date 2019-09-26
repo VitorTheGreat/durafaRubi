aa377e351d91b44e065dab7f6b1336e743cb5821
Vitor Hugo <vitor.services@gmail.com>
2019-09-26 19:13:58 -0300

    get ids from colors and references

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -10,6 +10,7 @@ use App\Models\Referencia;
 use App\Models\Tamanho;
 use App\Models\Tipo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProdutoController extends Controller
 {
@@ -65,12 +66,41 @@ class ProdutoController extends Controller
         $quantidade = request()->validate([
             'quantidade' => 'required',
         ]);
+
         // dd($data['idtamanho'], $data['quantidade']);
         //verify quantity and sizes
         foreach(array_combine($data['idtamanho'], $quantidade['quantidade']) as $idtamanho => $n ){
             if ($n != 0){
                 echo "id tamanho: ".$idtamanho." - Quantidade: ".$n."<br />";
-                $product = Produto::create($data);
+
+                $idreferencia = DB::select('SELECT idreferencia FROM referencia WHERE referencia = :referencia', ['referencia' => $data["referencia"]]);
+                $idcor = DB::select('SELECT idcor FROM cor WHERE nome = :cor', ['cor' => $data["cor"]]);
+                // var_dump($idreferencia[0]->idreferencia);
+                // var_dump($idcor[0]->idcor);
+                // echo 'id Ref: ' . $idreferencia[0]->idreferencia . '<br />';
+                // echo 'id Cor: ' . $idcor[0]->idcor;
+
+                $produto = Produto::create([
+                    'descricao' => request('descricao'),
+                    'marca' => request('marca'),
+                    'preco_venda' => request('preco_venda'),
+                    'preco_compra' => request('preco_compra'),
+                    'id_estoques' => request('id_estoques'),
+                    'referencia' => $idreferencia[0]->idreferencia,
+                    'idtamanho' => $idtamanho,
+                    'id_fornecedor' => request('id_fornecedor'),
+                    'id_tipo' => request('id_tipo'),
+                    'cor' => $$idcor[0]->idcor,
+                ]);
+
+                var_dump($produto);
+                // $idproduto = Product::where('idproduto', $produto);
+                // dd($idproduto);
+                // echo $idproduto;
+                // foreach ($idproduto as $idpro) {
+                    // echo "id tamanho: ".$idtamanho." - Quantidade: ".$n."<br />";
+                    // echo "Id produto: ".$idpro['idproduto']."<br />";
+                // }
             }
         }
         // return redirect('product');
